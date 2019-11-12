915f96e0a1af8619a557cf5e772cb22e3604d5e0
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2019-11-11 21:50:32 -0700

    API updates link and flash mv

--- src/sl_config.h
+++ src/sl_config.h
@@ -6,12 +6,15 @@
 #ifndef SL_CONFIG_H_
 #define SL_CONFIG_H_
 
-#define SL_CONFIG_VERSION_STRING "0.0"
-#define SL_CONFIG_VERSION_BCD 0x00
-#define SL_CONFIG_DOCUMENT_ID "unpublished"
-#define SL_CONFIG_NAME "unnamed"
-#define SL_CONFIG_TYPE "unknown"
-#define SL_CONFIG_HARDWARE_ID_STRING "0x00000000"
+#define SL_CONFIG_VERSION_STRING "0.14"
+#define SL_CONFIG_VERSION_BCD 0x0E
+#define SL_CONFIG_DOCUMENT_ID "s61eUqIGIdpmv1hJkhBH"
+#define SL_CONFIG_TEAM_ID ""
+#define SL_CONFIG_NAME "Nucleo-F722ZE"
+#define SL_CONFIG_TYPE "os"
+#define SL_CONFIG_PUBLISHER "Stratify Labs, Inc"
+#define SL_CONFIG_PERMISSIONS "public"
+#define SL_CONFIG_HARDWARE_ID_STRING "0x00000018"
 
 #endif
 
